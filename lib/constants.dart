01508b8fd8f98478035dc2b16d8191890be61e0f
SebasVergara <svergarap@unal.edu.co>
2021-10-12 21:46:22 -0500

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -6,5 +6,4 @@ const String REST_URL_PREFIX = '_api';
 
 const String VIDEO_URL = "https://pix.wix.com/services/youtube/playlistitems?maxResults=10&playlistId=UUPRRW41oBX43h9suAcufLBg";
 
-//const String LIVE_URL = "https://youtube.googleapis.com/youtube/v3/search?part=snippet%2Cid&channelId=UCj6PcyLvpnIRT_2W_mwa9Aw&eventType=live&maxResults=1&order=date&type=video&key=AIzaSyBMDLUoNw8wwbDlpDm9Pp3b3BiW2mJbepY";
-const String LIVE_URL = "https://youtube.googleapis.com/youtube/v3/search?part=snippet%2Cid&channelId=UCPRRW41oBX43h9suAcufLBg&eventType=live&maxResults=1&order=date&type=video&key=AIzaSyBMDLUoNw8wwbDlpDm9Pp3b3BiW2mJbepY";
+const String LIVE_URL = "https://youtube.googleapis.com/youtube/v3/search?part=snippet%2Cid&channelId=UCPRRW41oBX43h9suAcufLBg&eventType=live&maxResults=1&order=date&type=video&key={{API_KEY}}";
